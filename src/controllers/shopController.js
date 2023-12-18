398be0efa9e76f390f187824e490185a2692c338
nadiamorganti <nadiamorganti6@gmail.com>
2023-12-18 12:26:51 -0300

    Termino challange #5

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,136 +1,140 @@
 const path = require('path');
 
+const data = [
+    {
+       product_id: 1,
+       licence_name: "Pokemon",
+       category_name: "Figuras coleccionables",
+       product_name: "Pidgeotto",
+       product_description: "Figura coleccionable pokemon",
+       product_price: 1799.99,
+       dues: 10,
+       product_sku: "PKM001001",
+       img_front: "/multimedia/pokemon/pidgeotto-1.webp",
+       img_back: "/multimedia/pokemon/pidgeotto-box.webp"
+    },
+
+    {
+       product_id: 2,
+       licence_name: "Pokemon",
+       category_name: "Figuras coleccionables",
+       product_name: "charmander",
+       product_description: "Figura coleccionable pokemon",
+       product_price: 1999.99,
+       dues: 6,
+       product_sku: "PKM001002",
+       img_front: "/multimedia/star-wars/baby-yoda-1.webp",
+       img_back: "/multimedia/star-wars/baby-yoda-box.webp"
+    },
+
+    {
+        product_id: 3,
+        licence_name: "Pokemon",
+        category_name: "Figuras coleccionables",
+        product_name: "Pidgeotto",
+        product_description: "Figura coleccionable pokemon",
+        product_price: 1799.99,
+        dues: 10,
+        product_sku: "PKM001001",
+        img_front: "/multimedia/pokemon/pidgeotto-1.webp",
+        img_back: "/multimedia/pokemon/pidgeotto-box.webp"
+    },
+
+    {
+        product_id: 4,
+        licence_name: "Pokemon",
+        category_name: "Figuras coleccionables",
+        product_name: "Pidgeotto",
+        product_description: "Figura coleccionable pokemon",
+        product_price: 1799.99,
+        dues: 10,
+        product_sku: "PKM001001",
+        img_front: "/multimedia/pokemon/pidgeotto-1.webp",
+        img_back: "/multimedia/pokemon/pidgeotto-box.webp"
+    },
+
+    {
+        product_id: 5,
+        licence_name: "Pokemon",
+        category_name: "Figuras coleccionables",
+        product_name: "Pidgeotto",
+        product_description: "Figura coleccionable pokemon",
+        product_price: 1799.99,
+        dues: 10,
+        product_sku: "PKM001001",
+        img_front: "/multimedia/pokemon/pidgeotto-1.webp",
+        img_back: "/multimedia/pokemon/pidgeotto-box.webp"
+    },      
+                  
+    {
+        product_id: 6,
+        licence_name: "Pokemon",
+        category_name: "Figuras coleccionables",
+        product_name: "Pidgeotto",
+        product_description: "Figura coleccionable pokemon",
+        product_price: 1799.99,
+        dues: 10,
+        product_sku: "PKM001001",
+        img_front: "/multimedia/pokemon/pidgeotto-1.webp",
+        img_back: "/multimedia/pokemon/pidgeotto-box.webp"
+    },
+                        
+   {
+        product_id: 7,
+        licence_name: "Pokemon",
+        category_name: "Figuras coleccionables",
+        product_name: "Pidgeotto",
+        product_description: "Figura coleccionable pokemon",
+        product_price: 1799.99,
+        dues: 10,
+        product_sku: "PKM001001",
+        img_front: "/multimedia/pokemon/pidgeotto-1.webp",
+        img_back: "/multimedia/pokemon/pidgeotto-box.webp"
+    },
+
+    {
+        product_id: 8,
+        licence_name: "Pokemon",
+        category_name: "Figuras coleccionables",
+        product_name: "Pidgeotto",
+        product_description: "Figura coleccionable pokemon",
+        product_price: 1799.99,
+        dues: 10,
+        product_sku: "PKM001001",
+        img_front: "/multimedia/pokemon/pidgeotto-1.webp",
+        img_back: "/multimedia/pokemon/pidgeotto-box.webp"
+    },
+
+    {
+        product_id: 9,
+        licence_name: "Pokemon",
+        category_name: "Figuras coleccionables",
+        product_name: "Pidgeotto",
+        product_description: "Figura coleccionable pokemon",
+        product_price: 1799.99,
+        dues: 10,
+        product_sku: "PKM001001",
+        img_front: "/multimedia/pokemon/pidgeotto-1.webp",
+        img_back: "/multimedia/pokemon/pidgeotto-box.webp"
+    },
+
+                                    
+
+    ];
+
 module.exports = {
-    shop: (req, res) => {
-
-        const data = [
-            {
-            product_id: 1,
-            licence_name: "Pokemon",
-            category_name: "Figuras coleccionables",
-            product_name: "Pidgeotto",
-            product_description: "Figura coleccionable pokemon",
-            product_price: 1799.99,
-            dues: 10,
-            product_sku: "PKM001001",
-            img_front: "/img/pokemon/pidgeotto-1.webp",
-            img_back: "/img/pokemon/pidgeotto-box.webp"
-            },
-
-            {
-                product_id: 2,
-                licence_name: "Pokemon",
-                category_name: "Figuras coleccionables",
-                product_name: "charmander",
-                product_description: "Figura coleccionable pokemon",
-                product_price: 1999.99,
-                dues: 6,
-                product_sku: "PKM001002",
-                img_front: "/img/pokemon/charmander-1.webp",
-                img_back: "/img/pokemon/charmander-box.webp"
-                },
-
-                {
-                    product_id: 1,
-                    licence_name: "Pokemon",
-                    category_name: "Figuras coleccionables",
-                    product_name: "Pidgeotto",
-                    product_description: "Figura coleccionable pokemon",
-                    product_price: 1799.99,
-                    dues: 10,
-                    product_sku: "PKM001001",
-                    img_front: "/img/pokemon/pidgeotto-1.webp",
-                    img_back: "/img/pokemon/pidgeotto-box.webp"
-                    },
-
-                    {
-                        product_id: 1,
-                        licence_name: "Pokemon",
-                        category_name: "Figuras coleccionables",
-                        product_name: "Pidgeotto",
-                        product_description: "Figura coleccionable pokemon",
-                        product_price: 1799.99,
-                        dues: 10,
-                        product_sku: "PKM001001",
-                        img_front: "/img/pokemon/pidgeotto-1.webp",
-                        img_back: "/img/pokemon/pidgeotto-box.webp"
-                        },
-
-                        {
-                            product_id: 1,
-                            licence_name: "Pokemon",
-                            category_name: "Figuras coleccionables",
-                            product_name: "Pidgeotto",
-                            product_description: "Figura coleccionable pokemon",
-                            product_price: 1799.99,
-                            dues: 10,
-                            product_sku: "PKM001001",
-                            img_front: "/img/pokemon/pidgeotto-1.webp",
-                            img_back: "/img/pokemon/pidgeotto-box.webp"
-                            },      
-                          
-                            {
-                                product_id: 1,
-                                licence_name: "Pokemon",
-                                category_name: "Figuras coleccionables",
-                                product_name: "Pidgeotto",
-                                product_description: "Figura coleccionable pokemon",
-                                product_price: 1799.99,
-                                dues: 10,
-                                product_sku: "PKM001001",
-                                img_front: "/img/pokemon/pidgeotto-1.webp",
-                                img_back: "/img/pokemon/pidgeotto-box.webp"
-                                },
-                                
-                                {
-                                    product_id: 1,
-                                    licence_name: "Pokemon",
-                                    category_name: "Figuras coleccionables",
-                                    product_name: "Pidgeotto",
-                                    product_description: "Figura coleccionable pokemon",
-                                    product_price: 1799.99,
-                                    dues: 10,
-                                    product_sku: "PKM001001",
-                                    img_front: "/img/pokemon/pidgeotto-1.webp",
-                                    img_back: "/img/pokemon/pidgeotto-box.webp"
-                                    },
-
-                                    {
-                                        product_id: 1,
-                                        licence_name: "Pokemon",
-                                        category_name: "Figuras coleccionables",
-                                        product_name: "Pidgeotto",
-                                        product_description: "Figura coleccionable pokemon",
-                                        product_price: 1799.99,
-                                        dues: 10,
-                                        product_sku: "PKM001001",
-                                        img_front: "/img/pokemon/pidgeotto-1.webp",
-                                        img_back: "/img/pokemon/pidgeotto-box.webp"
-                                        },
-
-                                        {
-                                            product_id: 1,
-                                            licence_name: "Pokemon",
-                                            category_name: "Figuras coleccionables",
-                                            product_name: "Pidgeotto",
-                                            product_description: "Figura coleccionable pokemon",
-                                            product_price: 1799.99,
-                                            dues: 10,
-                                            product_sku: "PKM001001",
-                                            img_front: "/img/pokemon/pidgeotto-1.webp",
-                                            img_back: "/img/pokemon/pidgeotto-box.webp"
-                                            },
-
-                                            
-
-            ];
-
+    shop: (req, res) => {        
         res.render(path.resolve(__dirname, '../views/shop/shop.ejs'), {
             data
         });
     },
     item: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/shop/item.ejs'));
+        const itemId = req.params.id;
+        const item = data.find(item => item.product_id == itemId);
+
+        res.render(path.resolve(__dirname, '../views/shop/item.ejs'), {
+            item
+        });
 
     },
     cart: (req, res) => res.send('Esta es la vista del CARRITO'),
